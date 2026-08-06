72260d3b01347c00e19afac8bb20a1719af1d978
suhaniNale <nalesuhani16@gmail.com>
2026-08-06 19:14:38 +0530

    BST Insersion, del, Min, Max

--- Tree/BST/BST_print.cpp
+++ Tree/BST/BST_print.cpp
@@ -17,6 +17,39 @@ class Node {
         }
 };
 
+//inorder
+void inorder(Node * root){
+    if(root== NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout<< root->data<<" ";
+    inorder(root->right);
+}
+
+//preorder
+void preorder(Node * root){
+    if(root==NULL){
+        return;
+    }
+
+    cout<< root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+//postorder
+void postorder(Node * root){
+    if(root==NULL){
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout<< root->data<<" ";
+}
+
 //LOT
 void levelorder(Node * root){
 
@@ -66,6 +99,70 @@ Node* insertToBST(Node* &root, int d){
 
     return root;
 }
+//minimum value
+Node* minVal(Node* &root)
+{
+    Node* temp = root;
+
+    while(temp ->left != NULL){
+        temp = temp->left;
+    }
+    return temp;
+}
+//maximum value
+Node* maxVal(Node* &root){
+    Node* temp = root;
+
+    while(temp->right != NULL){
+        temp = temp -> right;
+    }
+    return temp;
+}
+
+//delete node
+Node* deleteFromBST(Node* root, int val){
+    //base case 
+    if(root== NULL){
+        return root;
+    }
+
+    if(root->data == val){
+        //case 1 ->zero child
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+        //case 2 -> left child present
+        if(root->left != NULL && root->right == NULL){
+            Node * temp = root->left;
+            delete root;
+            return temp;
+        }
+        //case 3 -> right child present
+        if(root->left == NULL && root->right != NULL){
+            Node * temp = root->right;
+            delete root;
+            return  temp;
+        }
+        //case 4 -> left and right  child present
+        if(root->left != NULL && root->right != NULL){
+            int mini = minVal(root->right) ->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+    else if(root->data > val){
+        //left part
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else{
+        //right part
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
 
 void takeInput(Node* &root){
     int data;
@@ -84,8 +181,40 @@ int main() {
     cout<<"enter data to create BST : ";  //50 30 70 20 40 60 80 -1
     takeInput(root);
 
-    cout<<"printing the BST output : "<<endl;
+    cout<<"\nprinting the BST output : "<<endl;
     levelorder(root);
+
+    cout<<"\nBST Inorder Traversal: ";
+    inorder(root);
+
+    cout<<"\nBST preorder Traversal: ";
+    preorder(root);
+
+    cout<<"\nBST postorder Traversal: ";
+    postorder(root);
+
+    cout<<"\nMinimum value :"<< minVal(root) ->data << endl;
+    
+    cout<<"\nMaximum value :" << maxVal(root) ->data <<endl;;
+
+    root = deleteFromBST(root, 30);
+
+    cout<<"\nprinting the BST output : "<<endl;
+    levelorder(root);
+
+    cout<<"\nBST Inorder Traversal: ";
+    inorder(root);
+
+    cout<<"\nBST preorder Traversal: ";
+    preorder(root);
+
+    cout<<"\nBST postorder Traversal: ";
+    postorder(root);
+
+    cout<<"\nMinimum value :"<< minVal(root) ->data << endl;
+    
+    cout<<"\nMaximum value :" << maxVal(root) ->data <<endl;;
+    
 
     
     return 0;
