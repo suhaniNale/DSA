b105bcbff3f885adbebd9e2262736189aff7f9b3
suhaniNale <nalesuhani16@gmail.com>
2026-09-23 19:20:47 +0530

    DSA practice que

---
+++ DSA_practice/Leaders_in_arr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+#include <climits> 
+
+using namespace std;
+
+vector<int> findLeaders(vector<int> &arr, int n){
+
+    vector<int> ans;
+    int maxi = INT_MIN;
+    
+    n = arr.size();
+
+    for(int i = n-1; i>=0 ; i--){
+        if(arr[i] > maxi){
+            ans.push_back(arr[i]);
+        }
+        //keep track of maxi elem
+        maxi = max(maxi, arr[i]);
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {16,17,4,3,5,2};
+    int n = arr.size();
+
+    vector<int> leaders = findLeaders(arr,n);
+
+    cout<<"Leaders in an Array : ";
+    for(int x : leaders){
+        cout<< x << " ";
+    }
+
+    return 0;
+
+}
